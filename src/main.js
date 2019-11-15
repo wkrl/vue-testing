3dc452183641a2502396f0a37d670a42f619216c
karl <karl_brendel@yahoo.de>
2019-11-15 21:41:32 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import TextHighlight from 'vue-text-highlight';
+import '@/assets/css/tailwind.css'
+ 
 Vue.config.productionTip = false
+Vue.component('text-highlight', TextHighlight);
 
 new Vue({
   router,
